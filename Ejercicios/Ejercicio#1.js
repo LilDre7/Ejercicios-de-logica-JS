d9f5dfac3bb6071f0d6ff6c9643d9c21cb22049e
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-03-17 21:48:50 -0600

    Terminando ejercicios

--- Ejercicios/Ejercicio#1.js
+++ Ejercicios/Ejercicio#1.js
@@ -3,95 +3,99 @@
 
 
 // Escribe un programa de una sola línea que escriba en la pantalla un texto que diga “Hello World” (document.write).
-document.write("Hello Word" , "<br>" , "<br>")
+document.write("Hello Word", "<br>", "<br>")
 
 
 // Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
-document.write( 3 + 5 , "<br>" , "<br>" );
+document.write(3 + 5, "<br>", "<br>");
 
 
 // Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga “Hola nombreUsuario”
-// let nombre = prompt("Escribe tu nombre: ");
-// document.write(`Tu nombre es ${nombre} un saludo: ${nombre} `)
+let nombre = prompt("Escribe tu nombre: ");
+document.write(`Tu nombre es ${nombre} un saludo: ${nombre} `)
+
 
 // Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
-// let n1 = prompt("Escribe un número");
-// let n2 = prompt("Escribe otro número");
-// document.write("La suma es: " + ( parseInt(n1)+ parseInt(n2) ) );
+let n1 = prompt("Escribe un número");
+let n2 = prompt("Escribe otro número");
+document.write("La suma es: " + (parseInt(n1) + parseInt(n2)));
+
 
 // Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
-// let number1  = prompt("Dime un numero:")
-// let number2 = prompt("Dime otro numero:")
-// if ( number1 >= number2){
-//     document.write(`El primer numero es mayor porque era el numero:  ${number1} 
-//     y tu coloste como segunda opcion el numero ${number2} `)
-// }else{
-//     document.write(`El segundo numero es mayor porque era el numero ${number2}
-//     y tu coloste como primera opcion el numero ${number2} ` )
-// }
+let number1 = prompt("Dime un numero:")
+let number2 = prompt("Dime otro numero:")
+if (number1 >= number2) {
+     document.write(`El primer numero es mayor porque era el numero:  ${number1} 
+     y tu coloste como segunda opcion el numero ${number2} `)
+} else {
+     document.write(`El segundo numero es mayor porque era el numero ${number2}
+     y tu coloste como primera opcion el numero ${number2} `)
+}
 
 // Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
-// let opcion1 = prompt("Dime un numero soldado: ");
-// let opcion2 = prompt("Dime tu segundo numero soldado: ")
-// let opcion3 = prompt("Dime tu tercer numero pelado: " )
-// if ( opcion1 > opcion2 && opcion1 > opcion3 ){
-//     document.write("El numero mayor de estos 3 datos fue la opcion")
-// }else if (opcion2 > opcion3) {
-//     document.write(opcion2)
-// }else {
-//     document.write(opcion3)
-// }
+let opcion1 = prompt("Dime un numero soldado: ");
+let opcion2 = prompt("Dime tu segundo numero soldado: ")
+let opcion3 = prompt("Dime tu tercer numero pelado: ")
+if (opcion1 > opcion2 && opcion1 > opcion3) {
+     document.write("El numero mayor de estos 3 datos fue la opcion")
+} else if (opcion2 > opcion3) {
+     document.write(opcion2)
+} else {
+     document.write(opcion3)
+}
 
 
 // Escribe un programa que pida un número y diga si es divisible por 2
-// let op1 = prompt("Dime un numero" );
-// if (op1 % 2){
-//     document.write( "Es divisible por 2" )
-// }else {
-//     document.write( "No es divisible por 2" )
-// }
+let op1 = prompt("Dime un numero");
+if (op1 % 2) {
+     document.write("Es divisible por 2")
+} else {
+     document.write("No es divisible por 2")
+}
 
 // Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
-// let frase = prompt( "Escribe tu frase favorita" );
-// let caracter = "a";
-// let contadores = 0;
-// for (let i = 0; i < frase.length; i++ ){
-//     if (frase.charAt(i) === caracter )
-//     contadores++;
-// }
-// document.write(`En tu frase tuvimos una cantidad de letra a de  ${contadores}` )
+let frase = prompt("Escribe tu frase favorita");
+let caracter = "a";
+let contadores = 0;
+for (let i = 0; i < frase.length; i++) {
+     if (frase.charAt(i) === caracter)
+          contadores++;
+}
+document.write(`En tu frase tuvimos una cantidad de letra a de  ${contadores}`)
 
 // Escribe un programa que pida una frase y escriba las vocales que aparecen
-// function vowelCounter(str) {
-//     let allVowels = 'aeiouAEIOU';
-//     let vCounter = 0;
+function vowelCounter(str) {
+     let allVowels = 'aeiouAEIOU';
+     let vCounter = 0;
 
-//     for(let i = 0; i < str.length; i++) {
-//         if(allVowels.indexOf(str[i]) !== -1) {
-//         console.log("The value of str[i] is: " + str[i]);
-//         vCounter += 1;
-//         }   
-//     }
-//     return vCounter;
-//     }
-//     console.log(vowelCounter("Hello YoUtUbe"));
+     for (let i = 0; i < str.length; i++) {
+          if (allVowels.indexOf(str[i]) !== -1) {
+               console.log("The value of str[i] is: " + str[i]);
+               vCounter += 1;
+          }
+     }
+     return vCounter;
+}
+console.log(vowelCounter("Hello YoUtUbe"));
 
 
 /*    Method nº 2:
 Javascript exercise: How to count the number of vowels in a
 string using Javascript.
 */
-// let allVowels = ["a", "e", "i", "o", "u"];
-// function vowelCounter(str) {
-//     let vCounter = 0;
 
-//     for (let i of str.toLowerCase()){
-//         if (allVowels.includes(i)) { vCounter++;
-//         }
-//     }
-//     console.log(`Your original string is: \n ${str} \n `);
-//     console.log(`The text contains ${vCounter} vowel(s)`);
-//     return vCounter;
-// }
+let allVowels = ["a", "e", "i", "o", "u"];
+function vowelCounter(str) {
+     let vCounter = 0;
+
+     for (let i of str.toLowerCase()) {
+          if (allVowels.includes(i)) {
+               vCounter++;
+          }
+     }
+     console.log(`Your original string is: \n ${str} \n `);
+     console.log(`The text contains ${vCounter} vowel(s)`);
+     return vCounter;
+}
 
 
--- Ejercicios/Ejercicos#7.js
+++ Ejercicios/Ejercicos#7.js
@@ -52,7 +52,7 @@ const palabraMasLarga = (cadenaDetexto) => {
 
      return palabras.reduce((a, b) => {
           return b.length > a.length ? b : a;
-     } , "" );
+     }, "");
 
 };
 
@@ -73,31 +73,160 @@ const encontrarCadena = largeText(cadenaDetexto)
 console.log(encontrarCadena);
 
 
-
-
 // Crea una función que reciba un arreglo de números y un número n, y devuelva un nuevo arreglo con los elementos mayores que n.
 
-const numeros = [ 1, 3 , 5 , 19 , 12 , 73 , 2]
+const numeros = [1, 3, 5, 19, 12, 73, 2]
 const n = 5;
 
-const numeroMayor = (numeros , n) => {
+const numeroMayor = (numeros, n) => {
      const result = numeros.filter(numero => numero > n);
      return result
 }
 
-const getNumber = numeroMayor(numeros , n);
+const getNumber = numeroMayor(numeros, n);
 console.log(getNumber);
-
-
 
 
 
 // Crea una función que reciba un arreglo de objetos con la siguiente estructura: {nombre: 'nombre', edad: 20} y devuelva la edad promedio.
 
+const student = [
+     { nombre: 'nombre', edad: 20 },
+     { nombre: 'nombre', edad: 24 },
+     { nombre: 'nombre', edad: 19 },
+     { nombre: 'nombre', edad: 54 },
+     { nombre: 'nombre', edad: 63 },
+     { nombre: 'nombre', edad: 12 },
+     { nombre: 'nombre', edad: 15 }
+];
+
+const edadPromedio = (student) => {
+     const edades = student.map(estudiante => estudiante.edad)
+     const sumaEdad = edades.reduce((total, edad) => total + edad);
+     const promedio = sumaEdad / edades.length;
+     return promedio.toFixed(0)
+}
+
+const edadPromedioDelosEstudiantes = (edadPromedio(student))
+console.log(edadPromedioDelosEstudiantes);
+
+
+
 // Crea una función que reciba una cadena de texto y devuelva la misma cadena pero con las palabras en orden inverso.
+
+const wordreverse = "La programacion es dificil pero es muy hermosa"
+
+const alreves = (wordreverse) => {
+     const cadenitaAlreves = wordreverse.split("").reverse().join("")
+     return cadenitaAlreves
+};
+
+const reverseElTexto = (alreves(wordreverse));
+console.log(reverseElTexto);
+
 
 // Crea una función que reciba un arreglo de números y devuelva el producto de todos los elementos.
 
+const sumNumeros = [36, 27, 33, 55, 72, 53, 48, 53, 12];
+
+const sumNumbers = (sumNumeros) => {
+     const sumarse = sumNumeros.reduce((num, sum) => num + sum);
+     return sumarse
+}
+
+const resultadoDelaSuma = (sumNumbers(sumNumeros));
+console.log(resultadoDelaSuma);
+
+
 // Crea una función que reciba un arreglo de objetos con la siguiente estructura: {nombre: 'nombre', edad: 20} y devuelva un nuevo arreglo con los nombres de las personas mayores de edad.
 
+const personasMayores = [
+     { nombre: 'nombre', edad: 20 },
+     { nombre: 'nombre', edad: 12 },
+     { nombre: 'nombre', edad: 13 },
+     { nombre: 'nombre', edad: 19 },
+     { nombre: 'nombre', edad: 13 },
+     { nombre: 'nombre', edad: 21 },
+     { nombre: 'nombre', edad: 64 },
+     { nombre: 'nombre', edad: 34 },
+     { nombre: 'nombre', edad: 18 },
+];
+
+const mayoresDeEdad = (personasMayores) => {
+     const person = personasMayores.map(element => element.edad).filter(a => a >= 18)
+     return person
+};
+
+const entranSoloMayoresDeEdad = (mayoresDeEdad(personasMayores));
+console.log(entranSoloMayoresDeEdad);
+
+
 // Crea una función que reciba una cadena de texto y devuelva la cantidad de veces que aparece cada letra en la cadena.
+
+const letra = "Soy Alvaro desarrollador de Software";
+
+// const letraForSeach = (letra) => {
+
+//      const frase = letra.split(" ")
+//      const letraGet = []
+
+//      for (i in frase) {
+//           const letraX = frase[i];
+
+//           for (j in letraX) {
+//                const letraY = letraX[j];
+
+//                if (letraY in letraGet) {
+//                     letraGet[letraY] += 1;
+//                } else {
+//                     letraGet[letraY] = 1;
+//                }
+//           }
+//      }
+
+//      return letraGet;
+
+//   };
+// Segunda forma mas facil o bueno maso menos
+
+const contar_letras = (letra) => {
+     const letraGet = {};
+
+     const partirLetras = letra.split(" ");
+
+     const letraFilter = partirLetras.filter(x => x.match(/[a-z]/i));
+
+     letraFilter.forEach(x => {
+          x = x.toLowerCase();
+          letraGet[x] = (letraGet[x] || 0) + 1;
+     })
+
+     return letraGet
+}
+
+
+const letraRepetida = (contar_letras(letra));
+console.log(letraRepetida);
+
+
+
+
+function contarLetras(letra) {
+     const letraGet = {};
+
+     for (let i = 0; i < letra.length; i++) {
+          const letraActual = letra[i].toLowerCase();
+
+          if (letraActual.match(/[a-z]/i)) {
+               if (letraActual in letraGet) {
+                    letraGet[letraActual] += 1;
+               } else {
+                    letraGet[letraActual] = 1;
+               }
+          }
+     }
+
+     return letraGet;
+}
+const letraRepetida2 = (contarLetras(letra));
+console.log(letraRepetida2);
