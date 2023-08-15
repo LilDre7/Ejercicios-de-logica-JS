c2c36636fa2d22317c0c1fec2f579f3f8d7f800a
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-08-15 13:44:02 -0600

    :memo: upadate document

--- Ejercicios/Ejercicio#16.js
+++ Ejercicios/Ejercicio#16.js
@@ -4,37 +4,26 @@
  * - La función recibirá el mes y el año y retornará verdadero o falso.
  */
 
-let mes = 1;
-let año = 2020;
-// Función existeViernes13(mes, año):
-const existeViernes13 = (mes, año) => {
-  // Paso 1: Obtener el día de la semana para el 13 de ese mes y año
-  let díaDeLaSemana13 = obtenerDíaDeLaSemana(13, mes, año);
-  // Paso 2: Verificar si el día de la semana es viernes (5) y retornar verdadero o falso
-  if (díaDeLaSemana13 === 5) {
-    return true;
-  } else {
-    false;
-  }
+function detectarViernes13(mes, año) {
+  const fecha = new Date(año, mes - 1, 13);
+  return fecha.getDay() === 5;
+}
+
+console.log(detectarViernes13(10, 2023));
+
+/*
+ * Dada una URL con parámetros, crea una función que obtenga sus valores.
+ * No se pueden usar operaciones del lenguaje que realicen esta tarea directamente.
+ *
+ * Ejemplo: En la url https://retosdeprogramacion.com?year=2023&challenge=0
+ * los parámetros serían ["2023", "0"]
+ */
+
+const url = "https://retosdeprogramacion.com?year=2023&challenge=0&perfil=120";
+
+const searchUrl = (url) => {
+  const paramValor = url.split("?")[1].split("&");
+  const paramValors = paramValor.map((param) => param.split("=")[1]);
+  return paramValors;
 };
-
-//     si díaDeLaSemana13 es igual a 5 entonces
-//         retornar verdadero
-//     sino
-//         retornar falso
-
-// Fin Función
-
-// Función obtenerDíaDeLaSemana(día, mes, año):
-// Algoritmo de Zeller para calcular el día de la semana
-//     si mes es igual a 1 o mes es igual a 2 entonces
-//         mes = mes + 12
-//         año = año - 1
-
-//     k = año % 100
-//     j = año / 100
-//     díaDeLaSemana = (día + (13 * (mes + 1)) / 5 + k + (k / 4) + (j / 4) + 5 * j) % 7
-
-//     retornar díaDeLaSemana
-
-// Fin Función
+console.log(searchUrl(url));
