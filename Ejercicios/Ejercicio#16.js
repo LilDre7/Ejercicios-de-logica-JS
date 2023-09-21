00cb344ede93329d8049e42ccd5abc254e2c1fd0
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-21 16:47:49 -0600

    :construction: Work in progress

--- Ejercicios/Ejercicio#16.js
+++ Ejercicios/Ejercicio#16.js
@@ -28,27 +28,31 @@ const searchUrl = (url) => {
 };
 console.log(searchUrl(url));
 
-Ejercicio 1: Encontrar el número mayor
+/* Ejercicio 1: Encontrar el número mayor
 Escribe una función que tome un arreglo de números como argumento y devuelva el número más grande en el arreglo.
-
 Consejo: Puedes usar un bucle para recorrer el arreglo y mantener un seguimiento del número más grande a medida que avanzas.
+*/
 
+/*
 Ejercicio 2: Contar palabras en una cadena
 Escribe una función que cuente cuántas palabras hay en una cadena de texto. Puedes asumir que las palabras están separadas por espacios.
-
 Consejo: Puedes dividir la cadena en un arreglo usando split(' ') y luego contar cuántos elementos hay en ese arreglo.
+*/
 
+/*
 Ejercicio 3: Comprobar si una palabra es un palíndromo
 Escribe una función que determine si una palabra es un palíndromo (se lee igual de izquierda a derecha y viceversa), ignorando los espacios y mayúsculas/minúsculas.
-
 Consejo: Puedes eliminar los espacios y convertir la palabra a minúsculas o mayúsculas para simplificar la comparación.
+*/
 
+/*
 Ejercicio 4: Calcular el factorial de un número
 Escribe una función que calcule el factorial de un número entero. El factorial de un número es el producto de todos los enteros positivos menores o iguales a ese número.
-
 Consejo: Puedes usar un bucle para calcular el factorial. Asegúrate de manejar el caso especial de 0.
+*/
 
+/*
 Ejercicio 5: Encontrar números primos
 Escribe una función que tome un número como argumento y determine si es primo (solo divisible por 1 y por sí mismo).
-
 Consejo: Puedes usar un bucle para verificar si el número es divisible por algún otro número entre 2 y su raíz cuadrada (para mejorar la eficiencia).
+*/
